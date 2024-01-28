c635a245bdfb8ed5fe5ceb3a56aeac41dd8c3622
Nodirbek <no0404ir@gmail.com>
2024-01-28 19:25:58 +0500

    cxssss

--- src/main/java/com/company/botadminpanel/dto/AddSectionDTO.java
+++ src/main/java/com/company/botadminpanel/dto/AddSectionDTO.java
@@ -1,5 +1,7 @@
 package com.company.botadminpanel.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,6 +12,8 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AddSectionDTO {
+    @NotBlank
     String name;
+    @NotNull
     Integer bookId;
 }
