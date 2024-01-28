a7dabe50884d1e1c6aec5c88cd7f6874b31a5287
Nodirbek <no0404ir@gmail.com>
2024-01-28 19:28:35 +0500

    vbghjkl

--- src/main/java/com/company/botadminpanel/dto/StoryDTO.java
+++ src/main/java/com/company/botadminpanel/dto/StoryDTO.java
@@ -1,5 +1,7 @@
 package com.company.botadminpanel.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class StoryDTO {
+    @NotNull
     Integer id;
+    @NotBlank
     String body;
+    @NotBlank
     String score;
+    @NotNull
     SectionDTO sectionDTO;
+    @NotNull
     UserDTO userDTO;
 }
